a8b5e699dff2b10d885dfaa8cb227d66752713df
superseun <67711095+Superseun99@users.noreply.github.com>
2024-09-10 22:19:09 +0100

    Update main.tf

--- section1/terraform/main.tf
+++ section1/terraform/main.tf
@@ -1,8 +1,3 @@
-# main.tf
-
-
-# terraform-modules/gcp-vpc/main.tf
-
 module "network" {
   source  = "./modules/network"
   network_name = "my-test"
@@ -18,4 +13,18 @@ module "gce" {
    subnet_name = module.network.subnet_names[0]
   machine_type = "e2-medium"
   zone = "us-east1-c"
+
+  service_account {
+    email  = "defaultserviceaccount@codematic-external-sandbox.iam.gserviceaccount.com"
+    scopes = [
+
+               "https://www.googleapis.com/auth/devstorage.read_only",
+               "https://www.googleapis.com/auth/logging.write",
+               "https://www.googleapis.com/auth/monitoring.write",
+               "https://www.googleapis.com/auth/service.management.readonly",
+               "https://www.googleapis.com/auth/servicecontrol",
+               "https://www.googleapis.com/auth/trace.append",
+            ]
+  }
+
 }
